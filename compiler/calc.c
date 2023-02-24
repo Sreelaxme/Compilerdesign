fa947279eded5c9084994961ee056e91f978885c
Sreelakshmi <112001042@smail.iitpkd.ac.in>
2023-02-24 14:27:24 +0530

    repo reorganised

---
+++ compiler/calc.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "calc3.h"
+#include "y.tab.h"
+static int lbl;
+int ex(nodeType *p) {
+	 int lbl1, lbl2;
+	 if (!p) return 0;
+	 switch(p->type) {
+		 case typeCon:
+		 return p->con.value;
+
+
+		 case typeOpr:
+		 switch(p->opr.oper) {
+		 case '+': return ex(p->opr.op[0]) + ex(p->opr.op[1]);
+		 case '-': return ex(p->opr.op[0]) - ex(p->opr.op[1]);
+		 case '*': return ex(p->opr.op[0]) * ex(p->opr.op[1]);
+		 case '/': return ex(p->opr.op[0]) / ex(p->opr.op[1]);
+		 }
+	 }
+	 return 0;
+ }
