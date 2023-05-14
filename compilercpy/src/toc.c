9a8689799caa8e4f18e4893f19a61a299c728dbd
Sreelakshmi <112001042@smail.iitpkd.ac.in>
2023-05-14 11:41:19 +0530

    working good...Project done ig

--- compilercpy/src/toc.c
+++ compilercpy/src/toc.c
@@ -36,18 +36,14 @@ void toC(node *p) {
  	case typeOpr:
  	switch(p->opr.oper) {
 		case '+': fprintf(stdout,"(");toC(p->opr.op[0]); fprintf(stdout," + "); toC(p->opr.op[1]);fprintf(stdout,")");/* fprintf(")");*/return ;
-<<<<<<< HEAD
-		case '-':/*fprintf(stdout,"("); */toC(p->opr.op[0]); fprintf(stdout," - "); toC(p->opr.op[1]);/*fprintf(stdout,")");*/return ;
-=======
 		case '-':fprintf(stdout,"("); toC(p->opr.op[0]); fprintf(stdout," - "); toC(p->opr.op[1]);fprintf(stdout,")");return ;
->>>>>>> temp
 		case '*': fprintf(stdout,"(");toC(p->opr.op[0]); fprintf(stdout," * "); toC(p->opr.op[1]);fprintf(stdout,")");return ;
 		case '/': fprintf(stdout,"(");toC(p->opr.op[0]); fprintf(stdout," / "); toC(p->opr.op[1]);fprintf(stdout,")"); return ;
         case '<': fprintf(stdout,"(");toC(p->opr.op[0]); fprintf(stdout," < "); toC(p->opr.op[1]);fprintf(stdout,")"); return ;
         case '>': fprintf(stdout,"(");toC(p->opr.op[0]);fprintf(stdout," > "); toC(p->opr.op[1]);fprintf(stdout,")");return ;
         case '%': fprintf(stdout,"(");toC(p->opr.op[0]);  fprintf(stdout," %% ");toC(p->opr.op[1]);fprintf(stdout,")");return ;
    
-		case '=':  toC(p->opr.op[0]); fprintf(stdout," = ");toC(p->opr.op[1]);/* fprintf(stdout,";"); *//*fprintf("\n");*/return ;
+		case '=':  toC(p->opr.op[0]); fprintf(stdout," = ");toC(p->opr.op[1]); fprintf(stdout,";"); /*fprintf("\n");*/return ;
 		case PRINT :  {
             fprintf(stdout,"printf(\"\ ");
             //int n =lengthOfArgList(p->opr.op[0]);
@@ -179,7 +175,7 @@ void toC(node *p) {
 		case STMNT: { 
 						toC(p->opr.op[0]); 
 						fprintf(stdout,"\n"); toC(p->opr.op[1]);
-                        toC()return; 
+                        fprintf(stdout,";\n");return; 
 					}
 		// case ARRAY_DECLARE : fprintf("ARR VAR "); fprintf(" %d", p->opr.op[1]->con.value);/*toC(p->opr.op[0]);fprintf(")");*/return ;
 		case ARRAY_ASSIGN : toC(p->opr.op[0]); fprintf(stdout,"[");toC(p->opr.op[1]);fprintf(stdout,"] = "); toC(p->opr.op[2]);fprintf(stdout,";\n");return ;
